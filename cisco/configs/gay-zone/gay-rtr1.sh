f097d9eb2556036a2ec0d6bfefdc5b459287f0fa
SantaSpeen <SantaSpeen@yandex.ru>
2022-04-23 01:06:18 +0300

    gay-zone basic config

---
+++ cisco/configs/gay-zone/gay-rtr1.sh
@@ -0,0 +1,64 @@
+en
+	er s
+	del vlan.dat
+
+	conf t
+
+		vl 22 
+			na "Native VLAN"
+		vl 44
+			na "Managment VLAN"
+
+		vl 220
+			na "VLAN1 Gay"
+		vl 221
+			na "VLAN2 Gay"
+		vl 222
+			na "VLAN3 Sexshop"
+		vl 223
+			na "VLAN4 Bezruk steet"
+		vl 224
+			na "VLAN5 Bezruk steet"
+		vl 225
+			na "VLAN6 Kazahstan"
+		vl 226
+			na "VLAN7 Kazahstan"
+		vl 227
+			na "VLAN8 Kazahstan"
+		vl 228
+			na "VLAN9 Sysadmin central"
+		vl 229
+			na "VLAN10 Sysadmin central"
+
+		lin c 0
+			exe 0 0
+			pass pass
+			login
+
+		lin v 0 15
+			exe 0 0
+			pass cisco
+			login local
+			t i s
+
+		int r gi0/0-1
+			no sh
+			sw m t
+			sw t n vl 22
+			exit
+
+		h gay-rtr1
+		ip domain n gay-rtr1
+		ena s cisco
+		ser p 
+		ba m "Hello from gay-rtr1!"
+
+		cr k g r
+		1024
+
+		ip s v 2
+		u admin s admin
+
+		do wr
+		do rel
+
